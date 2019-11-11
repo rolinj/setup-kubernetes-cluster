096cd44082732588a85e72733344543115a44720
rolinj <38485411+rolinj@users.noreply.github.com>
2019-11-11 10:55:12 +0800

    Update echo messages

--- kubernetes-setup.sh
+++ kubernetes-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Performing setup for kubernetes cluster."
-echo "Adding master and worker details to host file."
+echo "Performing setup for kubernetes cluster..."
+echo "Adding master and worker details to host file..."
 # Save initial inputs to variables.
 MASTER_HOSTNAME=$1
 MASTER_IPADDR=$2
@@ -23,34 +23,34 @@ echo "Updating packages..."
 # yum -y update
 echo "Updating packages complete!"
 
-echo "Disabling SELinux policy persistently"
+echo "Disabling SELinux policy persistently..."
 # Disable SELinux policy persistently
 setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
 echo "SELinux disabled successfully!"
 
-echo "Ensuring traffic will not be block by iptables."
+echo "Ensuring traffic will not be block by iptables..."
 # Ensure traffic is route correctly and not blocked by iptables.
 modprobe br_netfilter
 echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables 
 echo '1' > /proc/sys/net/ipv4/ip_forward
 echo "Iptables updated successfully!"
 
-echo "Disabling swap for kubelet to work properly"
+echo "Disabling swap for kubelet to work properly..."
 # Disabling swap for the kubelet to work properly.
 swapoff -a
 sed -i -r 's/(.+ swap .+)/#\1/' /etc/fstab
 echo "Swap disabled successfully!"
 cat /etc/fstab
 
-echo "Installing docker and its dependencies"
+echo "Installing docker and its dependencies..."
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum -y install docker-ce-18.09.9-3.el7
 echo "Installed docker successfully"
 docker version
 
-echo "Installing kubelet, kubeadm and kubectl"
+echo "Installing kubelet, kubeadm and kubectl..."
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -66,16 +66,16 @@ kubelet --version
 kubeadm version
 kubectl version
 
-echo "Starting and enabling docker/kubelet services."
+echo "Starting and enabling docker/kubelet services..."
 systemctl start docker && systemctl enable docker
 systemctl start docker && systemctl enable kubelet
 echo "Started and enabled docker/kubelt services upon boot up successfully!"
 
-echo "Pulling images that will be need by the cluster."
+echo "Pulling images that will be need by the cluster..."
 kubeadm config images pull
 echo "Pulled necessary images successfully!"
 
-echo "Setting default routing to our private network"
+echo "Setting default routing to our private network..."
 yum -y install net-tools
 route del default
 route add default ${ETH1_GATEWAY} eth1
