814cdf65b6a3e93c38f39612c925479d09088fd1
rolinj <>
2019-11-11 11:22:57 +0800

    Rename worker script and swap echo to printf

--- kubernetes-worker-setup.sh
+++ kubernetes-worker-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Performing setup for kubernetes cluster..."
-echo "Adding master and worker details to host file..."
+printf "\nAdding master and worker details to host file..."
 # Save initial inputs to variables.
 MASTER_HOSTNAME=$1
 MASTER_IPADDR=$2
@@ -18,12 +18,12 @@ echo "${IPADDR} ${HOSTNAME} ${HOSTNAME}" >> /etc/hosts # Add worker records to h
 echo "/etc/hosts file has been updated successfully!"
 cat /etc/hosts
 
-echo "Updating packages..."
+printf "\nUpdating packages..."
 # Perform package updates
 # yum -y update
 echo "Updating packages complete!"
 
-echo "Disabling SELinux policy persistently..."
+printf "\nDisabling SELinux policy persistently..."
 # Disable SELinux policy persistently
 setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
@@ -36,14 +36,14 @@ sed -i -r 's/(.+ swap .+)/#\1/' /etc/fstab
 echo "Swap disabled successfully!"
 cat /etc/fstab
 
-echo "Installing docker and its dependencies..."
+printf "\nInstalling docker and its dependencies..."
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum -y install docker-ce-18.09.9-3.el7
 echo "Installed docker successfully"
 docker version
 
-echo "Installing kubelet, kubeadm and kubectl..."
+printf "\nInstalling kubelet, kubeadm and kubectl..."
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -57,31 +57,30 @@ yum -y install kubelet kubeadm kubectl
 echo "Installed kubelet, kubeadm and kubectl successfully"
 kubelet --version
 kubeadm version
-kubectl version
 
-echo "Starting and enabling docker/kubelet services..."
+printf "\nStarting and enabling docker/kubelet services..."
 systemctl start docker && systemctl enable docker
 systemctl start docker && systemctl enable kubelet
 echo "Started and enabled docker/kubelt services upon boot up successfully!"
 
-echo "Pulling images that will be need by the cluster..."
+printf "\nPulling images that will be need by the cluster..."
 kubeadm config images pull
 echo "Pulled necessary images successfully!"
 
-echo "Setting default routing to our private network..."
+printf "\nSetting default routing to our private network..."
 yum -y install net-tools
 route del default
 route add default gw ${ETH1_GATEWAY} eth1
 echo "Updated default interface and gateway successfully!"
 ip route
 
-echo "Ensuring traffic will not be block by iptables..."
+printf "\nEnsuring traffic will not be block by iptables..."
 # Ensure traffic is route correctly and not blocked by iptables.
 modprobe br_netfilter
 echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables 
 echo '1' > /proc/sys/net/ipv4/ip_forward
 echo "Iptables updated successfully!"
 
-echo "Restarting kubelet.service..."
+printf "\nRestarting kubelet.service..."
 systemctl restart kubelet
 echo "Restarted kubelet service successfully!"
