5112b16f38179073b88a74b86ebd2c67eb4d671b
nachee1325 <chris.aguinaldo13@gmail.com>
2019-12-05 05:33:56 -0800

    update ntp download and settings

--- kubernetes-worker-setup.sh
+++ kubernetes-worker-setup.sh
@@ -67,6 +67,24 @@ printf "\nPulling images that will be need by the cluster...\n"
 kubeadm config images pull
 echo "Pulled necessary images successfully!"
 
+printf "\nSet time and date to local rtc and set ntp to yes...\n"
+timedatectl set-local-rtc yes
+timedatectl set-ntp yes
+echo "Successfully set to local rtc and set ntp!"
+
+printf "\nDownloading ntp and ntpdate for time configuration to sync...\n"
+yum install -y ntp ntpdate
+echo "Downloaded ntp and ntpdate succesfully!"
+
+printf "\nStart ntp service and set date to centos ntp pool...\n"
+systemctl start ntpd
+systemctl enable ntpd
+ntpdate -u -s 0.centos.pool.ntp.org 1.centos.pool.ntp.org 2.centos.pool.ntp.org
+systemctl restart ntpd
+echo "Setting of ntpdate succesfully set to centos pool ntp org!"
+
+timedatectl
+
 printf "\nSetting default routing to our private network...\n"
 yum -y install net-tools
 route del default
